41407fd2927f1942a33469758088ed0def8a850a
kaziashik <kazia096@gmail.com>
2021-10-05 18:19:32 +0800

    simple react test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <LodaUsers></LodaUsers>
+
+      <Person brand="apple" price="5000"></Person>
+      <Person brand="Microsoft" price="1000"></Person>
+      <Person brand="google" price="100"></Person>
+      <Person></Person>
     </div>
   );
+}
+
+function LodaUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+
+  return (
+    <div><h1>Users Loaded:{users.length}</h1>
+      {
+        users.map(user => <User name={user.name} phone={user.phone}></User>)
+      }
+    </div>
+  )
+
+}
+
+function User(props) {
+  return (
+    <div className="user">
+      <h2>Name:{props.name}</h2>
+      <p>call me baby!!{props.phone}</p>
+    </div>
+  )
+
+}
+
+function Person(props) {
+
+  const [points, setPoints] = useState(1);
+
+
+
+  const myStyle = {
+    background: 'lightblue',
+    border: '3px  solid  blue',
+    margin: '10px',
+    padding: '5px',
+    borderRadius: '10px'
+
+  }
+
+  const handleAddpoints = () => {
+    const newPints = points * 2;
+    setPoints(newPints);
+  }
+
+
+
+  return (<div style={myStyle}><h1>sakib  al hasan</h1>
+    <h4>Profession:Cricketer {props.brand}</h4>
+
+    <h5>asking money, price:{props.price}, I  have points: {points}</h5>
+
+    <p style={{ color: 'magenta', fontWeight: 'bold' }}>I CART WRITE MY OWN COMPONENT</p>
+
+    <button onClick={handleAddpoints}>Add points</button>
+  </div>
+  )
+
+
+
+
 }
 
 export default App;
